137db5f94872ca64014b8caa3c65f5bd241fb400
ChangJoo Park <pcjpcj2@gmail.com>
2016-10-31 17:15:34 +0900

    First Setups

--- config.rb
+++ config.rb
@@ -2,6 +2,9 @@
 # Page options, layouts, aliases and proxies
 ###
 
+# Timezon
+Time.zone = "Seoul"
+
 # Per-page layout changes:
 #
 # With no layout
@@ -28,17 +31,40 @@ end
 ###
 
 # Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  def get_taxonomies(slug = 'category')
+    list = []
+    blog.articles.select{ |i| i.data[slug].present? }.each do |article|
+      list = list.push article.data[slug]
+    end
+
+    return list.inject(Hash.new(0)){|hash, a| hash[a] += 1; hash}
+  end
+end
 
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+end
+
+
+# Blog
+activate :blog do |blog|
+  blog.sources   = "posts/:year-:month-:day-:title.html"
+  blog.paginate = true
+  blog.per_page = 6
+  blog.permalink = "blog/{year}/{month}/{day}/{title}"
+  blog.layout = "blog_layout"
+  blog.summary_separator = /(READMORE)/
+  blog.tag_template = "tag.html"
+  blog.custom_collections = {
+    category: {
+      link: '/categories/{category}',
+      template: '/category.html'
+    }
+  }
 end
